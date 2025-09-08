f0c48efdf90ea8a90e10f631a9c3202452f94ddb
Danilo Ferreira <danilo231105@gmail.com>
2025-09-08 20:25:34 -0300

    exercicio na pasta ConstrutorFuncionario

--- ConstrutorFuncionario/Funcionario.cs
+++ ConstrutorFuncionario/Funcionario.cs
@@ -4,6 +4,38 @@ namespace ConstrutorFuncionario
 {
     public class Funcionario
     {
-        
+        //atributos com encapsulamento
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public double Salario { get; set; }
+
+        //construtores
+        public Funcionario()
+        {
+        }
+        public Funcionario(int codigo)
+        {
+            Codigo = codigo;
+        }
+        public Funcionario(int codigo, string nome)
+        {
+            Codigo = codigo;
+            Nome = nome;
+        }
+        public Funcionario(int codigo, string nome, double salario)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Salario = salario;
+        }
+
+
+        //métodos
+        public void MostrarAtributos()
+        {
+            Console.WriteLine("Código: " + Codigo + "\tNome: " + Nome
+            + "\tSalário R$ " + Salario);
+        }
+
     }
 }
--- ConstrutorFuncionario/Program.cs
+++ ConstrutorFuncionario/Program.cs
@@ -1,1 +1,10 @@
 ﻿using ConstrutorFuncionario;
+
+Funcionario c1 = new Funcionario();
+c1.MostrarAtributos();
+
+Funcionario c2 = new Funcionario(2, "Luna");
+c2.MostrarAtributos();
+
+Funcionario c3 = new Funcionario(3);
+c3.MostrarAtributos();
