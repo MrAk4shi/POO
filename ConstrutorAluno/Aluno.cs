55b2c4b24e7272ab11d06e36f89b5cc415be3092
Danilo Ferreira <danilo231105@gmail.com>
2025-09-20 11:48:40 -0300

    comecei ConstrutorAluno

---
+++ ConstrutorAluno/Aluno.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConstrutorAluno
+{
+    public class Aluno
+    {
+ 
+
+
+
+        public void MostrarAtributos()
+        {
+            Console.WriteLine("Nome: " + Nome +
+            "\tRA: " + Ra + "\tContador: " + Contador);
+        }
+    }
+}
