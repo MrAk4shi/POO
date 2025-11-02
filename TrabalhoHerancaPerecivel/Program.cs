8c8a0864d5604538e00855f4226dba06b4affe13
Danilo Ferreira <danilo231105@gmail.com>
2025-11-02 14:58:29 -0300

    Trabalho herança

---
+++ TrabalhoHerancaPerecivel/Program.cs
@@ -0,0 +1,10 @@
+﻿using TrabalhoHerancaPerecivel;
+
+Perecivel p = new Perecivel();
+p.Codigo = 1;
+p.Nome = "Carne";
+p.Preco = 50;
+p.DataFabricacao = "02/11/2025";
+p.DataValidade = "23/11/2025";
+p.Lote = 067;
+p.Mostrar();
